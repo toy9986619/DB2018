601f6c7eb7ccaff64285686b1a5c1ec94ef1ab6e
toy9986619 <toy9986619@gmail.com>
2018-11-12 17:32:11 +0800

    新增簡易認證測試
    1. 將User model 移至App/Models，同時修正config/auth.php 的users class namespace
    2. 修正users table，加入type 欄位，用於辨識使用者型態
    3. SuperUser 測試帳號 資料填充
    4. Route 定義相關認證測試
    5. Controller 定義相關認證測試 (make:auth 產生)
    6. View 加入相關認證測試 (make:auth 產生)

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
     /**
      * The attributes that are mass assignable.
      *
