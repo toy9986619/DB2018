ef0ee33a1a64ad6ec8d0d0d17355128c46435cf3
toy9986619 <toy9986619@gmail.com>
2018-12-13 23:33:38 +0800

    調整Vue 的使用方式，改為單一元件配合Blade，而非使用Vue-Router
    Vue-Router 的相關程式碼暫時註解
    App.vue 為搭配Vue-Router 使用，暫時放置

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,20 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+// import App from './App.vue'
+// import VueRouter from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
 
-import Gallery from './components/Gallery.vue'
+// import Gallery from './components/Gallery.vue'
+// import CardInfo from './components/CardInfo.vue'
 
-const router = new VueRouter({
-    mode: 'history',
-    base: __dirname,
-    routes: [
-        {path: '/gallery', component: Gallery}
-    ]
-})
+// const router = new VueRouter({
+//     mode: 'history',
+//     base: __dirname,
+//     routes: [
+//         {path: '/gallery', component: Gallery},
+//         {path: '/cardInfo/:id', component: CardInfo}
+//     ]
+// })
 
-new Vue(Vue.util.extend({ router }, App)).$mount('#app')
+// new Vue(Vue.util.extend({ router }, App)).$mount('#app')
+
+Vue.component('gallery', require('./components/Gallery.vue'));
+Vue.component('card_info', require('./components/CardInfo.vue'));    
+
+const app = new Vue({
+    el: '#app'
+});
